48fd8299913b331812ccb77e1425cf6b7a6bf799
Tinu Francis <tinufrancis21@gmail.com>
2026-03-23 17:43:21 +0530

    Initial commit after making changes

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,13 +1,19 @@
 package demo;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.ResourceBundle.Control;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.WebDriver.Window;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import java.util.logging.Level;
@@ -48,10 +54,67 @@ public class TestCases {
 
     // TODO: Write Test Cases Here
 
-     public void testCase01(){
-     System.out.println("Start Test case: testCase01");
-     driver.get("https:www.google.com");
-     System.out.println("end Test case: testCase02");
-     }
+    //Verify that the Stack Overflow homepage URL contains 'stackoverflow.'
+    public void testCase01(){
+    System.out.println("Start Test case: testCase01");
+    driver.get("https://stackoverflow.com/");
+    String url = driver.getCurrentUrl();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    WebElement popupElement = driver.findElement(By.id("onetrust-banner-sdk"));
+    if (popupElement.isDisplayed()){
+    WebElement acceptCookies = driver.findElement(By.id("onetrust-reject-all-handler"));
+    acceptCookies.click();
+    }
+    if(url.contains("stackoverflow")){
+    System.out.println("Test case passed. Url contains stackoverflow");
+    }
+    else{
+    System.out.println("Test case failed. Url does not contain stackoverflow");
+    }
+    System.out.println("end Test case: testCase01");
+    }
+
+
+
+    //Search for a Specific Topic and Verify Results
+    public void testCase02() throws InterruptedException{
+    System.out.println("Start Test case: testCase02");
+    driver.get("https://stackoverflow.com/");
+    Thread.sleep(3000);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    WebElement popupElement = driver.findElement(By.id("onetrust-banner-sdk"));
+    if (popupElement.isDisplayed()){
+    WebElement acceptCookies = driver.findElement(By.id("onetrust-reject-all-handler"));
+    acceptCookies.click();
+    }
+    Thread.sleep(3000);
+    WebElement searchBox = driver.findElement(By.xpath("//input[@name='q']"));
+    searchBox.sendKeys("Python list comprehension");
+
+    Actions actions = new Actions(driver);
+    actions.keyDown(Keys.ENTER).keyUp(Keys.ENTER).perform();
+    String searchedURL = driver.getCurrentUrl();
+
+    Thread.sleep(10000);
+
+
+    String mainWindow = driver.getWindowHandle();
+
+    driver.switchTo().newWindow(WindowType.TAB);
+    driver.navigate().to(searchedURL);
+    List<WebElement> answerElementTitles = driver.findElements(By.className("s-post-summary--content-title"));
+    for (int i=0; i<=2; i++){
+        String titleText = answerElementTitles.get(i).getText().toLowerCase();
+        if(titleText.contains("python") || titleText.contains("list") || titleText.contains("comprehension")){
+            System.out.println("Test case passed. Answer title contains one of the searched words");
+        }
+        else{
+            System.out.println("Test case failed. Answer title does not contain any of the searched words");
+        }
+    }
+
+    System.out.println("end Test case: testCase02");
+    }
+
 
 }
